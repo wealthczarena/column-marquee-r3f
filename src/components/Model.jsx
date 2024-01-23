21bdacb9faffec8878d717c8d3dd11964fe97e37
Czarena Wealth <wealthczarena@gmail.com>
2024-01-23 18:43:38 -0300

    model.jsx update

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ const Model = ({ instanceCount, spacing }) => {
     const model = useGLTF('./bunny_can.glb')
     const modelRef = useRef()
 
+    // Rotating the model
     useEffect(() => {
         const modelObject = modelRef.current
         modelObject.rotation.y = Math.PI
@@ -13,27 +14,24 @@ const Model = ({ instanceCount, spacing }) => {
         // console.log('Loaded model:', model)
     }, [model])
 
+    // Creating a grid with instancing
+    return <>
+        <InstancedMesh 
+        ref={modelRef} 
+        args={[
+            model.scene.children[0].geometry, 
+            model.scene.children[0].material,
+            instanceCount
+        ]} >
+            
+        </InstancedMesh>
 
-  return <>
-    <InstancedMesh 
-    ref={modelRef} 
-    args={[
-        model.scene.children[0].geometry, 
-        model.scene.children[0].material,
-        instanceCount
-    ]} >
-        
-    </InstancedMesh>
-
-
-
-
-    <primitive 
-        object={ model.scene } 
-        ref={ modelRef } 
-        scale={ 1, 1, 1 }
-    />
-  </>
+        <primitive 
+            object={ model.scene } 
+            ref={ modelRef } 
+            scale={ 1, 1, 1 }
+        />
+    </>
 }
 
 export default Model
